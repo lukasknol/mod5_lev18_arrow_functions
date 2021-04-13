2bbe426903d2de3f34a74b36c6a1b8cf7cc9d33d
Lukas Knol <lukasknol@gmail.com>
2021-04-13 10:26:52 +0200

    javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+// A. Question: zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions()
+
+  // A. Answer: zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log ("Joe, ik rock de arrow functions!")
+
+
+// B. Question
+const fivePlusSeven = function () {
+    return 5 + 7
+  };
+  
+  fivePlusSeven();
+
+// B. Answer
+const fivePlusSeven = function () => return 5 + 7
